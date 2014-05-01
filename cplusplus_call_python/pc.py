28f632082bd52b213e8ca0bd24988274e05adec8
no7dw <no7david@gmail.com>
2014-05-02 00:22:53 +0800

    c plus plus call python demo

---
+++ cplusplus_call_python/pc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+     
+
+     class Person:
+
+         def sayHi(self):
+
+             print 'Hi'
+
+     
+
+     class Second:
+
+         def invoke(self, obj):
+
+             obj.sayHi()
+
+         def method2(self, obj):
+
+             print "this is method2"
+
+     
+
+     def sayHi(name):
+
+         print 'Hi ,', name
