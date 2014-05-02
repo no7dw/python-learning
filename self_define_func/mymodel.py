1d2326b1090b30074a6b86c4447ca830189e7387
no7dw <no7david@gmail.com>
2014-05-03 00:24:52 +0800

    add self define function using import

---
+++ self_define_func/mymodel.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+#Filename:mymode.py
+
+version = '1.0'
+
+def sayHello():
+
+    print("Hello world")
+
+
+
+def modelName():
+
+    return __name__
+
+#end of model
---
+++ self_define_func/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import mymodel
+
+print(__name__)
+
+mymodel.sayHello()
+
+print('Version: ', mymodel.version)
+
+print('Model Name: ', mymodel.modelName())
